4d3ecd870809f9bb7393dc78d3f2a4b947e22924
Ndhossain <hhhssnahiid@gmail.com>
2022-06-06 23:12:15 +0600

    rgb to hex conversion

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// factorialize
+//problem 1: factorialize
 /*function factorialize(num) {
     if (num == 0) {
         return -1;
@@ -12,7 +12,7 @@
 let ans = factorialize(5);
 console.log(ans);
 
-// recursive factorialize
+// Problem 2: recursive factorialize
 function factorialize2(num2) {
     if (num2 === 0 || num2 === 1) {
         return 1;
@@ -26,7 +26,7 @@ function factorialize2(num2) {
 let ans2 = factorialize2(5);
 console.log(ans2);
 
-// while loop
+// problem 3 : while loop
 function factorialize3(num) {
     var result = num;
     if (num === 0 || num === 1) {
@@ -41,6 +41,8 @@ function factorialize3(num) {
 }
 let ans3 = factorialize3(5);
 console.log(ans3);*/
+
+// problem 4: fibonacci
 
 // if (num <= 1) {
 //     return 1;
@@ -85,7 +87,7 @@ console.log(ans3);*/
 // console.log(result);
 
 // hacker rank problem solving
-// problem:Plus Minus
+// problem 4 :Plus Minus
 
 // function plusMinus(arr) {
 //     let positive = 0;
@@ -110,7 +112,7 @@ console.log(ans3);*/
 // let mathArr = [-4, 3, -9, 0, 4, 1];
 // let result = plusMinus(mathArr);
 
-// problem:Mini-Max Sum
+// problem 6 :Mini-Max Sum
 
 // function minMax(arr){
 //   let max = arr[0];
@@ -132,7 +134,7 @@ console.log(ans3);*/
 // let mathArr = [1, 2, 3, 4, 5];
 // let result = minMax(mathArr);
 
-// problem:Time Conversion
+// problem 7 : Time Conversion
 
 // function timeConversion(s) {
 //     let amPm = s.charAt(8);
@@ -199,7 +201,7 @@ console.log(ans3);*/
 
 // leetcode problem solving
 
-// two-sum
+// problem 8 : two-sum
 
 // var twoSum = function (nums, target) {
 //     let arrayOfIndex = []
@@ -221,6 +223,8 @@ console.log(ans3);*/
 // var res = twoSum(numbers, targets);
 // console.log(res);
 
+// problem 9 : add two numbers
+
 // var addTwoNumbers = function (l1, l2) {
 //     let val1 = 0;
 //     let val2 = 0;
@@ -237,6 +241,8 @@ console.log(ans3);*/
 // let secondArray = [5, 6, 4];
 // let result = addTwoNumbers(firstArray, secondArray);
 // console.log(result)
+
+// problem 10 : to camel case
 
 // at first i have to convert the string in to a array
 // i have to iterate over the array and check the value that is it a dash/underscore
@@ -269,3 +275,107 @@ console.log(ans3);*/
 // }
 
 // console.log(toCamelCase("the-stealth-warrior"));
+
+// problem 11 : closest elevator
+
+// function elevator(left, right, call) {
+//     return Math.abs(call - right) > Math.abs(call - left) ? "left" : "right";
+// }
+
+// console.log(elevator(0, 2, 1));
+
+// problem 12: Vowel Count
+
+// function getCount(str) {
+//   let vowelsCount = 0;
+
+//   // enter your magic here
+//   const strArray = str.split("");
+//   strArray.forEach((element) => {
+//     if (
+//       element === "a" ||
+//       element === "e" ||
+//       element === "i" ||
+//       element === "o" ||
+//       element === "u"
+//     ) {
+//       vowelsCount += 1;
+//     }
+//   });
+
+//   return vowelsCount;
+// }
+
+// console.log(getCount("abracadabragi"));
+
+// problem 13 : maskify secure strings
+
+// function maskify(cc) {
+//   let newVal = "";
+//   const ccArray = cc.split("");
+//   ccArray.map((element, index) => {
+//     const ind = ccArray.length - 4;
+//     index < ind ? (newVal += "#") : (newVal += element);
+//   });
+//   return newVal;
+// }
+
+// console.log(maskify("4556364607935616"));
+
+// problem 14 : Find The Parity Outlier
+
+// function findOutlier(integers) {
+//   //your code here
+//   const even = [];
+//   const odd = [];
+//   integers.forEach(element => {
+//       element % 2 === 0 ? even.push(element) : odd.push(element);
+//   });
+//   return even.length === 1 ? even[0] : odd[0];
+// }
+
+// console.log(findOutlier([0, 1, 2]));
+
+// Split Strings
+
+// problem 15 : split strings
+
+// function solution(str) {
+//   const strArray = str.split("");
+//   const newArray = [];
+
+//   if (strArray.length % 2 !== 0) strArray.push("_");
+//   if (strArray.length === 0) return strArray;
+//   strArray.forEach((element, index) => {
+//     if (index % 2 === 0) {
+//       newArray.push(element + strArray[index + 1]);
+//     }
+//   });
+//   return newArray;
+// }
+
+// function solution(str) {
+//   return (str + "_").match(/../g) || [];
+// }
+// console.log(solution(""));
+
+// Problem 16: RGB To Hex Conversion
+
+// function rgb(r, g, b) {
+//   // complete this function
+//   const res = [...arguments].map((element) =>
+//     element <= 0
+//       ? `00`
+//       : element >= 255
+//       ? `FF`
+//       : element < 16
+//       ? `0` + element.toString(16).toUpperCase()
+//       : element.toString(16).toUpperCase()
+//   );
+
+//   return res.reduce((prev, current) => (prev += current));
+// }
+
+// console.log(rgb(139, 145, 15));
+
+
